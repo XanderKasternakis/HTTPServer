3cd54f8427cea2177ff8fb1a1dcd392ed16b236d
BRANDON CHAN <bc780@scarletmail.rutgers.edu>
2023-11-14 16:39:08 -0500

    server shell

--- server.py
+++ server.py
@@ -1,13 +1,27 @@
 import socket, json, random, datetime, hashlib, sys
 
-#take in args
+class Server:
 
-ip = sys.argv[1]
-port = int(sys.argv[2])
-accFile = sys.argv[3]
-sesTimeout = int(sys.argv[4])
-rootDir = sys.argv[5]
+    def main(self):
+        #take in args
+        ip = sys.argv[1]
+        port = int(sys.argv[2])
+        accFile = sys.argv[3]
+        sesTimeout = int(sys.argv[4])
+        rootDir = sys.argv[5]
+        self.start()
 
+    def start(ip,port):
+        #start the server
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((ip,port))
+        s.listen(10)
+        
+        while True:
+            c, addr = s.accept()
 
+    def post():
+
+    def get():
 
 
