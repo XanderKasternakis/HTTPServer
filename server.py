d767e5505567810f43bdeae14fe108b069ac48b5
BRANDON CHAN <bc780@scarletmail.rutgers.edu>
2023-11-14 16:29:38 -0500

    take in args

--- server.py
+++ server.py
@@ -2,3 +2,12 @@ import socket, json, random, datetime, hashlib, sys
 
 #take in args
 
+ip = sys.argv[1]
+port = int(sys.argv[2])
+accFile = sys.argv[3]
+sesTimeout = int(sys.argv[4])
+rootDir = sys.argv[5]
+
+
+
+
